e10740fbbf701b3b3f1db66e13c10568c32938a9
Daniil Solopov <soldan-work@mail.ru>
2022-09-29 22:45:41 +0300

    Миграции, сидеры, модели

--- app/Http/Controllers/MyFirstController.php
+++ app/Http/Controllers/MyFirstController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MyFirstController extends Controller
 {
-    public function show($id = 0){
-        return view('first.show', ['var1' => $id, 'var2' => '2']);
+    public function show($id = 0)
+    {
+        $users = DB::table('users')->where('age', 0)->pluck('salary');
+        dump($users);
+        // 
+        // foreach ($posts as $post) 
+        // {
+        //     dump(gettype($post->title));
+        // }
+			
+        // return view('first.show', ['posts' => $posts]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,36 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
+/**
+    * Seed the application's database.
+        *
+        * @return void
      */
     public function run()
-    {
+    {   
+        DB::table('users')->insert([
+        'name' => Str::random(10),
+        'email' => Str::random(5), 
+        'age' => rand(0,99), 
+        'salary' => rand(30000, 240000), 
+        'created_at' => date("Y-m-d H:i:s"),
+        'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => Str::random(10),
+            'slug' => Str::random(5), 
+            'likes' => rand(30000, 240000),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"), 
+            ]);
         // \App\Models\User::factory(10)->create();
     }
 }
